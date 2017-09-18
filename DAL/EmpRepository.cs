11150a4e11dcc0986fc2f904f8746b31a7e6698e
MiteshGadhiya <m.thakkars@gmail.com>
2017-09-18 15:26:44 +0530

    initial commit
    initial commit

---
+++ DAL/EmpRepository.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.Core.Objects;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    public class EmpRepository
+    {
+        Sample_WebAPIEntities objSample_WebAPIEntities;
+        public EmpRepository()
+        {
+            objSample_WebAPIEntities = new Sample_WebAPIEntities();
+        }
+        public int? InsertEmp(string NAME)
+        {
+            ObjectParameter IsSaved = new ObjectParameter("IsSaved", typeof(int)); 
+            objSample_WebAPIEntities.InsertEmp(NAME,IsSaved);
+            return Convert.ToInt32(IsSaved.Value);
+        }
+        public List<GetAllEmps_Result> GetAllEmps()
+        {
+            return objSample_WebAPIEntities.GetAllEmps().ToList();
+        }
+    }
+}
